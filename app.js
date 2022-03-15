e5d381e03d36c572822335e8ca46c4ee23c30f50
HONGMINJU <mhong0800@gmail.com>
2022-03-15 17:01:04 +0900

    feat : socket.io 시작 (connection 구현)

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const socketio = require('socket.io')
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json({limit : '50mb'}));
+app.use(bodyParser.urlencoded({limit : '50mb', extended: true}));
+app.use(cookieParser());
+app.use(express.json());
+
+var server = app.listen(3001,()=>{
+    console.log('Server is running on port number 3000 (채팅서버)')
+})
+
+var io = socketio.listen(server);
+
+io.on('connection',function(socket) {
+    console.log(`HIHIHIHI\nConnection : SocketId = ${socket.id}`)
+    var userName = '';
+})
+
+module.exports = app;
