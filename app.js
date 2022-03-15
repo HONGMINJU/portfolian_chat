1fea128bd7fb49f053f266d7ed09d247abd6da7b
HONGMINJU <mhong0800@gmail.com>
2022-03-15 19:02:59 +0900

    feat : 특정 rooom에 메세지 전송

--- app.js
+++ app.js
@@ -4,29 +4,31 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
-const http = require('http').Server(app);
 const socketio = require('socket.io');
+var server = app.listen(3001,()=>{
+    console.log('Server is running on port number 3001 (채팅서버)')
+})
+const io = socketio(server,{ path: '/socket.io' });
 
 app.use(bodyParser.json({limit : '50mb'}));
 app.use(bodyParser.urlencoded({limit : '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.json());
 
-var server = app.listen(3001,()=>{
-    console.log('Server is running on port number 3001 (채팅서버)')
-})
-
-const io = socketio(server,{ path: '/socket.io' });
-
 io.on('connection',function(socket) {
     console.log(`Connection : SocketId = ${socket.id}`);
 
     socket.on('send', function(data) {
+
         console.log('send message client to server')
+
         const message_data = JSON.parse(JSON.stringify(data))
         const messageContent = message_data.messageContent;
-        console.log(`recieve message : ${messageContent}`)
-        io.emit('receive',{"messageContent" : messageContent});
+        const roomId = message_data.roomId;
+
+        console.log(`roomId : ${roomId} message : ${messageContent}`)
+        //io.emit('receive',{ "messageContent" : messageContent });
+        socket.broadcast.to(`${roomId}`).emit('receive', { "messageContent" : messageContent })
     })
 })
 
