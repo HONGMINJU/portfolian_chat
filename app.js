edd067b46da4137040c3f3b4640e35b90d3f9224
HONGMINJU <mhong0800@gmail.com>
2022-03-24 12:34:57 +0900

    fix : broadcast 에러 해결(socket->io)

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ io.on('connection',function(socket) {
         const roomId = message_data.roomId;
 
         console.log(`roomId : ${roomId} message : ${messageContent}`);
-        socket.broadcast.emit('receive', { "messageContent" : messageContent })
+        io.broadcast.emit('receive', { "messageContent" : messageContent })
     });
 
     socket.on('disconnect', function () {
